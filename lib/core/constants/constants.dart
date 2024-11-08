2896702893fba2588bd64269391e3a26f4e88c60
Nastya-Khairullina <nastya.maxnyova@mail.ru>
2024-11-08 11:41:36 +0300

    Add files via upload

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,3 @@
+const String homeUrl = 'http://run.mocky.io/v3/bbce1b05-7915-4454-92b7-fc74827f8793';
+const String current_productURL = 'http://run.mocky.io/v3/9e0adcd1-a291-4d20-a2d0-264972b411e2';
+
