1285a27b0465bdda01f9d8170de4d7f33d911efc
Nastya-Khairullina <nastya.maxnyova@mail.ru>
2024-11-08 11:40:38 +0300

    Delete lib directory

--- lib/feature/home/domain/repositories/home_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-import '../../../../core/error/failure.dart';
-import '../entities/homestore_entity.dart';
-
-abstract class HomeRepository {
-
-  Future<Either<Failure, List<HomeStoreEntity>>>? getHomestore();
-
-}
