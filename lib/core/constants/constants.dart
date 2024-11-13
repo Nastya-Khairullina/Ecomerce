e7e8b07f6b899fbf0209a8a740276ad05ee90657
Nastya-Khairullina <nastya.maxnyova@mail.ru>
2024-11-13 15:37:52 +0300

    Delete lib directory

--- lib/core/constants/constants.dart
+++
@@ -1,3 +0,0 @@
-const String homeUrl = 'https://run.mocky.io/v3/bbce1b05-7915-4454-92b7-fc74827f8793';
-const String current_productURL = 'https://run.mocky.io/v3/01f1e365-88b7-4229-8773-e219e2e1aad3';
-
